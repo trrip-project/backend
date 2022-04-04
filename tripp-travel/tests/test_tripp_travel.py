f643da242e2a8d3893a18dc9f9add61e5317a8d4
venky120041 <venky120041@gmail.com>
2022-04-04 22:38:11 +0530

    adding new functions

--- tripp-travel/tests/test_tripp_travel.py
+++ tripp-travel/tests/test_tripp_travel.py
@@ -5,4 +5,5 @@ def test_greeting():
     assert login.sayWelcome("Venkatesh") == "Hello Venkatesh"
 def test_version():
     assert __version__ == '0.1.0'
-
+def test_mul():
+    assert login.mul(2) == 4
--- tripp-travel/tripp_travel/login.py
+++ tripp-travel/tripp_travel/login.py
@@ -1,3 +1,7 @@
 def sayWelcome(name=None):
     message = "Hello user1" if name is None else f"Hello {name}"
     return message
+
+def mul(number=None):
+    return number * 2
+
